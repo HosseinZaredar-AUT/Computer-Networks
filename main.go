9922188ab9ad189fa68556b34ad0aeb0b3aa211d
Hossein <hossein.zare9978@gmail.com>
2020-07-22 21:12:15 +0430

    Reading cluster list from file

--- main.go
+++ main.go
@@ -1,9 +1,45 @@
 package main
 
 import (
+	"bufio"
+	"flag"
 	"fmt"
+	"os"
+	"strings"
 )
 
+func checkError(err error) {
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(-1)
+	}
+}
+
+func readClusterList(clusterList *[][2]string, listPath string) {
+	f, err := os.Open(listPath)
+	checkError(err)
+
+	defer func() {
+		f.Close()
+		checkError(err)
+	}()
+
+	s := bufio.NewScanner(f)
+	for s.Scan() {
+		fields := strings.Fields(s.Text())
+		*clusterList = append(*clusterList, [2]string{fields[0], fields[1]})
+	}
+
+	err = s.Err()
+	checkError(err)
+}
+
 func main() {
-	fmt.Println("Hello!")
+	listPath := flag.String("l", "", "cluster list file path")
+	// dirPath := flag.String("d", "", "directory path")
+	flag.Parse()
+
+	clusterList := make([][2]string, 0, 10)
+	readClusterList(&clusterList, *listPath)
+	fmt.Println(clusterList)
 }
